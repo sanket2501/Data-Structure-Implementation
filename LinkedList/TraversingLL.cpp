8331271aea8391ff8f8d46339be5f8e60d96d59d
sanket2501 <sanketshrawagi86999@gmail.com>
2022-03-20 23:15:41 +0530

    Iterative

---
+++ LinkedList/TraversingLL.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node* next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+void DisplayLL(node* head)
+{
+    node* curr=head;
+    while(curr!=NULL)
+    {
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+}
+int main()
+{
+    node* head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    head->next->next->next->next=new node(50);
+
+    DisplayLL(head);
+}
