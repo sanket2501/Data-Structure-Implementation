ffe729f32f2b23d89118b7dd509d4f06e73e3dc5
sanket2501 <sanketshrawagi86999@gmail.com>
2022-03-20 23:14:53 +0530

    Using Iterative

---
+++ LinkedList/Search.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node* next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+int SearchLL(node* head, int x)
+{
+    int pos=1;
+    node* curr=head;
+    while(curr!=NULL)
+    {
+        if(curr->data==x){
+            return pos;
+        }
+        else
+        {
+            curr=curr->next;
+            pos++;
+        }
+
+    }
+    return -1;
+}
+
+void printlist(node *head){
+    node *curr=head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }cout<<endl;
+}
+
+int main()
+{
+    node* head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    head->next->next->next->next=new node(50);
+    printlist(head);
+    cout<<"Position is "<<SearchLL(head,50);
+}
