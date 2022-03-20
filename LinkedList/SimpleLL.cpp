20268bfe384ba64fe6f7a6fdb983465b6e88eb15
sanket2501 <sanketshrawagi86999@gmail.com>
2022-03-20 23:15:27 +0530

    Implementation

---
+++ LinkedList/SimpleLL.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node* next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+int main()
+{
+    node* head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    head->next->next->next->next=new node(50);
+}
