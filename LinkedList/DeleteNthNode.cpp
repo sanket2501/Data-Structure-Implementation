ba53330a4f8abd02b9c8fa7ac0c58fc0e3e2aa55
sanket2501 <sanketshrawagi86999@gmail.com>
2022-03-22 18:16:52 +0530

    Linked List

---
+++ LinkedList/DeleteNthNode.cpp
@@ -0,0 +1,59 @@
+/*
+10 20 30 40 50 
+10 20 40 50
+*/
+
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node* next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+void DisplayLL(node* head)
+{
+    node* curr=head;
+    while(curr!=NULL)
+    {
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }cout<<endl;
+}
+void DeleteNthNode(node* head,int n)
+{
+    int len=0;
+    node*curr;
+    for(curr=head;curr!=NULL;curr=curr->next)
+        len++;
+    if(len<n)
+        return;
+    curr=head;
+    int i=1;
+    while(i<n-1 && curr->next!=NULL)
+    {
+        curr=curr->next;
+        i++;
+    }
+    node* temp=curr->next;
+    curr->next=curr->next->next;
+    delete(temp);
+}
+
+int main()
+{
+    node* head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    head->next->next->next->next=new node(50);
+
+    DisplayLL(head);
+    DeleteNthNode(head,3);
+    DisplayLL(head);
+
+}
